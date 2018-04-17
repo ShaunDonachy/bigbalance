0ae14db9ca07f350443faaf83499a93d31d82340
Mack Stump <mack.stump@gmail.com>
2018-04-17 15:35:02 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate memmap;
+
+use std::fs::OpenOptions;
+
+use memmap::MmapMut;
+
+fn init_balancesheet() {
+    const PATH: &str = "BALANCE";
+    let file = OpenOptions::new().read(true).write(true).create(true).open(&PATH).unwrap();
+    // 25 million users 4 bytes per balance
+    file.set_len(25_000_000 * 4).unwrap();
+
+    let mut mmap = unsafe { MmapMut::map_mut(&file).unwrap() };
+
+    for i in 0..25_000_000 {
+        mmap[i * 4 + 0] = 5;
+        mmap[i * 4 + 1] = 0;
+        mmap[i * 4 + 2] = 0;
+        mmap[i * 4 + 3] = 0;
+    }
+}
+
+fn main() {
+    init_balancesheet();
+}
